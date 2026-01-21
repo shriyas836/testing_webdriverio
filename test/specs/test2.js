1e777a033fff91888455ffc67a304f4a23479324
Shriya Sharma <142300940+shriyas836@users.noreply.github.com>
2026-01-20 22:58:22 -0800

    Refactor test case for Rahul Shetty website
    Refactor test for Rahul Shetty website to improve reliability and readability.

--- test/specs/test2.js
+++ test/specs/test2.js
@@ -34,38 +34,38 @@ import { expect } from 'chai'
    
 //     })
 // })
-describe('rahul shetty website',()=>
-{
-    it("rahul buttons",async()=>
-    {
-       // await browser.url("https://rahulshettyacademy.com/loginpagePractise")
-       await browser.url("/")
-        await $("#username").setValue("rahulshettyacademy")
-        await browser.pause(2000)
-        await $("#password").setValue("learning")
-        await browser.pause(2000)
-        const radioBu=await $$("input[type='radio']")
-         await radioBu[1].click()
+// describe('rahul shetty website',()=>
+// {
+//     it("rahul buttons",async()=>
+//     {
+//        // await browser.url("https://rahulshettyacademy.com/loginpagePractise")
+//        await browser.url("/")
+//         await $("#username").setValue("rahulshettyacademy")
+//         await browser.pause(2000)
+//         await $("#password").setValue("learning")
+//         await browser.pause(2000)
+//         const radioBu=await $$("input[type='radio']")
+//          await radioBu[1].click()
 
 
-        // await browser.waitUntil(async()=> await browser.isAlertOpen(),
-        //     {
-        //         timeout:6000,
-        //         timeoutMsg:"incorrect"
+//         // await browser.waitUntil(async()=> await browser.isAlertOpen(),
+//         //     {
+//         //         timeout:6000,
+//         //         timeoutMsg:"incorrect"
 
-        //     }
-        // )
-        await $(".modal-content").waitForDisplayed()
+//         //     }
+//         // )
+//         await $(".modal-content").waitForDisplayed()
 
-        await $("#cancelBtn").click()
-        await browser.pause(2000)
+//         await $("#cancelBtn").click()
+//         await browser.pause(2000)
 
-        await radioBu[1].click()
-        await $(".modal-content").waitForDisplayed()
+//         await radioBu[1].click()
+//         await $(".modal-content").waitForDisplayed()
 
 
-         await $("#okayBtn").click()
-         await browser.pause(2000)
+//          await $("#okayBtn").click()
+//          await browser.pause(2000)
 
 
 
@@ -77,5 +77,40 @@ describe('rahul shetty website',()=>
 
 
 
-    })
-})
+//     })
+// })
+describe('rahul shetty website', () => {
+    it("rahul buttons", async () => {
+
+        await browser.url("/");
+        await browser.pause(2000); // allow initial load (important for Jenkins)
+
+        const username = await $("#username");
+        await username.waitForDisplayed({ timeout: 10000 });
+        await username.setValue("rahulshettyacademy");
+
+        const password = await $("#password");
+        await password.waitForDisplayed({ timeout: 10000 });
+        await password.setValue("learning");
+
+        const radioBu = await $$("input[type='radio']");
+        await radioBu[1].waitForClickable({ timeout: 10000 });
+        await radioBu[1].click();
+
+        const modal = await $(".modal-content");
+        await modal.waitForDisplayed({ timeout: 10000 });
+
+        const cancelBtn = await $("#cancelBtn");
+        await cancelBtn.waitForClickable({ timeout: 10000 });
+        await cancelBtn.click();
+
+        await radioBu[1].click();
+        await modal.waitForDisplayed({ timeout: 10000 });
+
+        const okBtn = await $("#okayBtn");
+        await okBtn.waitForClickable({ timeout: 10000 });
+        await okBtn.click();
+
+    });
+});
+
