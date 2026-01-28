177fd98c43488d591098adc52b834da04cdee3ba
Shriya Sharma <142300940+shriyas836@users.noreply.github.com>
2026-01-28 01:13:17 -0800

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -145,8 +145,8 @@ exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     //services: ['visual'],
-    //services:['chromedriver'],
-    services:[],
+    services:['chromedriver'],
+   
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
